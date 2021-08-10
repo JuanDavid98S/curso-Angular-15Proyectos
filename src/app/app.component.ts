83190ea6f41c07293ff2df1517362a893e1f9722
Juan David <juadsanchezlon@unal.edu.co>
2021-08-10 09:19:20 -0500

    Curso Angular pra parte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'holaMundo';
+
+  //dataBinding
+  title = 'holaMundo'
+  nombre = 'Juan David Sánchez'
+
+  //PropertyBinding
+  textoPlaceHolder = 'Escribe algo porfi'
+  rutaImagen = "https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg"
+  texto = 'Aprendiendo angular, eventBinding'
+
+  //eventBinding
+  cambiarTexto(): void {
+    this.texto = 'Aprendiendo Angular, eventBinding, texto que cambia con botón'
+  }
+
+  //Two way binding
+  input = ""
+
+  //Directivas
+  listaEstudiantes: any[] = [
+    {nombre: 'Juan David', estado: 'Graduado'},
+    {nombre: 'Estebanano', estado: 'Graduado'},
+    {nombre: 'Gemelo', estado: 'Pailas'},
+    {nombre: 'Punko', estado: 'Pailas'},
+    {nombre: 'Loco', estado: 'Pailas'},
+    {nombre: 'otro', estado: 'Pailas'},
+    {nombre: 'Nicolás Gómez', estado: 'Libre'}
+  ]
+
+  mostrar = true
+
+  toggle():void {
+    this.mostrar = !this.mostrar
+  }
 }
